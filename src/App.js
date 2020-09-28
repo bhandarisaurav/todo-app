dc158bf1b287f8d71aaa63ca1134b9bcf6ebf35e
bhandarisaurav <sbhandari@deerhold.git>
2020-09-28 22:47:18 +0545

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+
+  formSubmitted = (event) => {
+    event.preventDefault();
+    let todo = event.target.newTodo.value;
+    event.target.newTodo.value = "";
+    this.setState({
+      todos: [...this.state.todos, { title: todo, done: false }],
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo.title}>{todo.title}</li>
+          ))}
+        </ul>
+        <form onSubmit={this.formSubmitted}>
+          <label forName="newTodo">Add Todo</label>
+          <input id="newTodo" name="newTodo" />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
