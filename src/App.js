900ea2cb3b65b5e3281068cd1d367a72e26ce34b
bhandarisaurav <sbhandari@deerhold.git>
2020-10-03 21:00:34 +0545

    :hammer: Added Bootstrap themes and added new layout

--- src/App.js
+++ src/App.js
@@ -1,35 +1,91 @@
 import React, { Component } from "react";
-
+import "./App.css";
+import {
+  Button,
+  Form,
+  Container,
+  Jumbotron,
+  ListGroup,
+  Badge,
+} from "react-bootstrap";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      todos: [],
+      newTodo: "",
+      todos: [{ title: "Default State", done: true }],
     };
   }
 
-  formSubmitted = (event) => {
-    event.preventDefault();
-    let todo = event.target.newTodo.value;
-    event.target.newTodo.value = "";
+  newTodoOnchange = (e) => {
     this.setState({
-      todos: [...this.state.todos, { title: todo, done: false }],
+      newTodo: e.target.value,
     });
   };
+
+  formSubmitted = (event) => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, { title: this.state.newTodo, done: false }],
+      newTodo: "",
+    });
+  };
+
+  toggleTodo = (e, i) => {
+    var newState = [...this.state.todos];
+    newState[i].done = !newState[i].done;
+    this.setState({
+      todos: newState,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <ul>
-          {this.state.todos.map((todo) => (
-            <li key={todo.title}>{todo.title}</li>
-          ))}
-        </ul>
-        <form onSubmit={this.formSubmitted}>
-          <label forName="newTodo">Add Todo</label>
-          <input id="newTodo" name="newTodo" />
-          <button type="submit">Add Todo</button>
-        </form>
-      </div>
+      <Container className="p-3 m-auto">
+        <Jumbotron className="w-75 p-5 mx-auto">
+          <h3>Create Todo</h3>
+
+          <Form className="w-50" onSubmit={this.formSubmitted}>
+            <Form.Group>
+              <Form.Label>Add Todo</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="New Todo"
+                name="newTodo"
+                onChange={this.newTodoOnchange}
+                value={this.state.newTodo}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </Form>
+        </Jumbotron>
+
+        <Jumbotron className="w-75 p-5 m-auto">
+          <h3>
+            All Todos <Badge variant="primary">{this.state.todos.length}</Badge>
+          </h3>
+          <ListGroup className="w-50">
+            {this.state.todos.map((todo, index) => (
+              <ListGroup.Item
+                key={index}
+                variant="primary"
+                className={todo.done ? "font-weight-bold p-0 checked" : "p-0"}
+              >
+                <input
+                  type="checkbox"
+                  className="m-3"
+                  defaultChecked={todo.done}
+                  onClick={(event) => this.toggleTodo(event, index)}
+                />
+                {todo.title}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Jumbotron>
+      </Container>
     );
   }
 }
